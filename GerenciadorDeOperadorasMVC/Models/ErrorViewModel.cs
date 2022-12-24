8585d1c7ed5898c920179b5cb9411d07d6823c1f
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2022-12-23 23:05:02 -0300

    Criação do projeto

---
+++ GerenciadorDeOperadorasMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GerenciadorDeOperadorasMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
