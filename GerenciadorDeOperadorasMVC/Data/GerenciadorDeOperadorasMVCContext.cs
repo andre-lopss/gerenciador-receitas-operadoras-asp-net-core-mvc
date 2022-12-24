d8b10f9e741f2e6350c094fe68580891dc0527fd
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2022-12-24 18:53:28 -0300

    CRUD Scaffolding

---
+++ GerenciadorDeOperadorasMVC/Data/GerenciadorDeOperadorasMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorDeOperadorasMVC.Models
+{
+    public class GerenciadorDeOperadorasMVCContext : DbContext
+    {
+        public GerenciadorDeOperadorasMVCContext (DbContextOptions<GerenciadorDeOperadorasMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GerenciadorDeOperadorasMVC.Models.Operadora> Operadora { get; set; }
+    }
+}
--- GerenciadorDeOperadorasMVC/Startup.cs
+++ GerenciadorDeOperadorasMVC/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using GerenciadorDeOperadorasMVC.Models;
 
 namespace GerenciadorDeOperadorasMVC
 {
@@ -33,6 +35,9 @@ namespace GerenciadorDeOperadorasMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<GerenciadorDeOperadorasMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("GerenciadorDeOperadorasMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
