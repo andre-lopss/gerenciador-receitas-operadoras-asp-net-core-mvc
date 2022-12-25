60196f6080227a0b56889005829fc907b44c92ed
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2022-12-25 14:07:55 -0300

    BeneficiarioFormularioViewModel e componente select para Operadoras

--- GerenciadorDeOperadorasMVC/Startup.cs
+++ GerenciadorDeOperadorasMVC/Startup.cs
@@ -44,6 +44,7 @@ namespace GerenciadorDeOperadorasMVC
 
             services.AddScoped<ServicoPopularBase>();
             services.AddScoped<BeneficiarioService>();
+            services.AddScoped<OperadoraService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
