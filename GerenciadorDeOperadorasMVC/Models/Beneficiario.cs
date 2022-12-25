0ceed0d7f530f10426274fbc24ef0f6f72e578c3
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2022-12-25 16:55:38 -0300

    Validação

--- GerenciadorDeOperadorasMVC/Models/Beneficiario.cs
+++ GerenciadorDeOperadorasMVC/Models/Beneficiario.cs
@@ -9,23 +9,23 @@ namespace GerenciadorDeOperadorasMVC.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
-        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
+        [Required(ErrorMessage = "{0} requirido")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} deve ter um tamanho entre {2} e {1}")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
-        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        [Required(ErrorMessage = "{0} requirido")]
+        [EmailAddress(ErrorMessage = "Entre com um email válido")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
+        [Required(ErrorMessage = "{0} requirido")]
         [Display(Name = "Data de Aniversário")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Aniversario { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
-        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
+        [Required(ErrorMessage = "{0} requirido")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} deve ser de {1} a {2}")]
         [Display(Name = "Valor do Plano")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double ValorPlano { get; set; }
