8187f862d63ab013d3e03695e3fcebb9503aa3a0
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2022-12-25 17:28:34 -0300

    Preparando a navegação de telas de pesquisa de planos

---
+++ GerenciadorDeOperadorasMVC/Services/RegistroPlanosService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeOperadorasMVC.Services
+{
+    public class RegistroPlanosService
+    {
+    }
+}
